1a9411b50f274a8ce406bb6363853027c8097dd0
tata2401 <itar64464@gmail.com>
2026-05-26 09:31:55 +0700

    Update index.php

--- login/crud2026 new/index.php
+++ login/crud2026 new/index.php
@@ -7,8 +7,6 @@ if (!isset($_SESSION['status']) || $_SESSION['status'] != "login") {
     exit();
 }
 
-// --- INI ANTI-BACK ---
-
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Pragma: no-cache");
 
